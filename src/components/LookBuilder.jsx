03981f63adebd6ae0d69925ae2f3768bc73be2c5
Patrick Hill <patrickrhill@gmail.com>
2021-08-21 19:30:42 -0400

    adds sub nav

--- src/components/LookBuilder.jsx
+++ src/components/LookBuilder.jsx
@@ -11,6 +11,30 @@ const LookBuilder = () => (
       <BigHeading>The Look Builder</BigHeading>
     </div>
 
+    <div class="container bg-gray-800 sm:sticky top-0 z-50 mb-64 sm:mb-128 flex flex-wrap gap-x-16 gap-y-8 mt-16">
+      <a class="inline-block text-gray-500 hover:text-white" href="#intro">
+        Intro
+      </a>
+      <a
+        class="inline-block text-gray-500 hover:text-white"
+        href="#exploration"
+      >
+        Exploration
+      </a>
+      <a class="inline-block text-gray-500 hover:text-white" href="#process">
+        Design
+      </a>
+      <a
+        class="inline-block text-gray-500 hover:text-white"
+        href="#prototyping"
+      >
+        Prototyping
+      </a>
+      <a class="inline-block text-gray-500 hover:text-white" href="#conclusion">
+        Conclusion
+      </a>
+    </div>
+
     <Footer />
   </>
 );
