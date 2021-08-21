37126dfbd5a57e9e4853f83d360b087ac53ae677
Patrick Hill <patrickrhill@gmail.com>
2021-08-21 19:28:48 -0400

    makes look builder page component

---
+++ src/components/LookBuilder.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import BigHeading from "./BigHeading";
+
+const LookBuilder = () => (
+  <>
+    <Header />
+
+    <div className="container">
+      <BigHeading>The Look Builder</BigHeading>
+    </div>
+
+    <Footer />
+  </>
+);
+
+export default LookBuilder;
