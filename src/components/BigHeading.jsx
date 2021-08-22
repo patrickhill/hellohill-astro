bc84dea7a26930c2b0a945414379d2fdea4fb985
Patrick Hill <patrickrhill@gmail.com>
2021-08-22 13:20:08 -0400

    responsive text size on headers

--- src/components/BigHeading.jsx
+++ src/components/BigHeading.jsx
@@ -1,11 +1,5 @@
 import React from "react";
 
-const BigHeading = ({ className = "", ...props }) => (
-  <div
-    className={`text-6xl font-thin leading-tight bg-gradient-brand text-clip ${className}`}
-  >
-    {props.children}
-  </div>
-);
+const BigHeading = ({ className = "", ...props }) => <div className={`text-4xl lg:text-6xl font-thin leading-tight bg-gradient-brand text-clip ${className}`}>{props.children}</div>;
 
 export default BigHeading;
--- src/components/SubHeading.jsx
+++ src/components/SubHeading.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-const SubHeading = ({ ...props }) => <div className="bg-gradient-brand text-clip uppercase text-lg font-black tracking-widest">{props.children}</div>;
+const SubHeading = ({ ...props }) => <div className="bg-gradient-brand text-clip uppercase text-normal md:text-lg font-black tracking-widest">{props.children}</div>;
 
 export default SubHeading;
