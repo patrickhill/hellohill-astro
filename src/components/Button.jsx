f13971654ee28a09e07791b33f9814c6720d096d
Patrick Hill <patrickrhill@gmail.com>
2021-08-22 15:05:21 -0400

    reduces button padding on mobile

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import { roundedClassNames } from "./roundedClassNames";
 
 const Button = ({ className = "", ...props }) => {
-  let baseClassNames = `${roundedClassNames} bg-gradient-brand bg-full hover:shadow-2xl hover:bg-expand transition-all duration-150 px-32 py-16 flex justify-center items-center font-light text-base`;
+  let baseClassNames = `
+    ${roundedClassNames}
+    bg-gradient-brand
+    bg-full
+    hover:shadow-2xl
+    hover:bg-expand
+    transition-all
+    duration-150
+    px-16 sm:px-32
+    py-8 sm:py-16
+    flex
+    justify-center
+    items-center
+    font-light text-base
+  `;
 
   return props.href ? (
     <a href={props.href} className={`${baseClassNames} ${className}`} {...props}>
