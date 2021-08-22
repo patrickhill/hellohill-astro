a7bb0107357c2472c6209d8750754487cf1adcf2
Patrick Hill <patrickrhill@gmail.com>
2021-08-22 13:24:22 -0400

    space adjust

--- src/components/BigHeading.jsx
+++ src/components/BigHeading.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-const BigHeading = ({ className = "", ...props }) => <div className={`text-4xl lg:text-6xl font-thin leading-tight bg-gradient-brand text-clip ${className}`}>{props.children}</div>;
+const BigHeading = ({ className = "", ...props }) => <div className={`text-3xl sm:text-4xl lg:text-5xl xl:text-6xl font-thin leading-tight bg-gradient-brand text-clip ${className}`}>{props.children}</div>;
 
 export default BigHeading;
