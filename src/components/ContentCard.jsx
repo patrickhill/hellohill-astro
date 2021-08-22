126cda23f87ba02e62aee274c705d12adebd65e0
Patrick Hill <patrickrhill@gmail.com>
2021-08-22 08:38:09 -0400

    extracts content card into component

---
+++ src/components/ContentCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const ContentCard = ({ className = "", ...props }) => {
+  return (
+    <div
+      className={`
+        bg-white
+        rounded-xl
+        shadow-2xl
+        p-32
+        space-y-32
+        w-full
+        max-w-3xl
+        mx-auto
+        sm:rounded-3xl sm:px-64 sm:pt-64 sm:pb-72
+        ${className}
+      `}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export default ContentCard;
--- src/components/LookBuilder.jsx
+++ src/components/LookBuilder.jsx
@@ -3,6 +3,7 @@ import Footer from "./Footer";
 import Header from "./Header";
 import BigHeading from "./BigHeading";
 import Button from "./Button";
+import ContentCard from "./ContentCard";
 
 const title = "The Look Builder";
 const desc = "Visualizing custom suit combinations";
@@ -39,7 +40,7 @@ const LookBuilder = () => (
     </div>
 
     <div className="space-y-64 sm:space-y-128 px-24 overflow-hidden">
-      <div className="bg-white rounded-xl sm:rounded-3xl shadow-2xl p-32 sm:px-64 sm:pt-64 sm:pb-72 space-y-32 w-full max-w-3xl mx-auto">
+      <ContentCard>
         <h2 className="mt-0" id="intro">
           Intro
         </h2>
@@ -58,9 +59,9 @@ const LookBuilder = () => (
           <h4>My Role:</h4>
           <p>UI Design, 3D Design, UI Dev, Design Leadership</p>
         </div>
-      </div>
+      </ContentCard>
 
-      <div className="bg-white rounded-xl sm:rounded-3xl shadow-2xl p-32 sm:px-64 sm:pt-64 sm:pb-72 space-y-32 w-full max-w-3xl mx-auto">
+      <ContentCard>
         <h2 id="exploration">Exploration</h2>
         <hr className="border-gray-300" />
 
@@ -68,7 +69,7 @@ const LookBuilder = () => (
         <p>The team knew from the get go that the hardest part would be the image creation process. It seemed like an insurmountable task. Gentux has 12 suits, 8 tuxes, 438 ties, 53 vests, 32 cummerbunds, 12 shirts, and 250+ other accessories. How do you get a photo of every conceivable combination for a customer to see?</p>
         <p> On top of that, how do you build a user interface that makes managing such a massive scale of items easy and intuitive?</p>
         <p>We ran through a barrage of tests to try and find something that would work.</p>
-      </div>
+      </ContentCard>
 
       <div>
         <div className="case-study-grid-images w-full max-w-[1182px] mx-auto">
@@ -114,16 +115,16 @@ const LookBuilder = () => (
         </div>
       </div>
 
-      <div className="bg-white rounded-xl sm:rounded-3xl shadow-2xl p-32 sm:px-64 sm:pt-64 sm:pb-72 space-y-32 w-full max-w-3xl mx-auto">
+      <ContentCard>
         <p>We developed a way to layer images so that items could be photographed separately and compiled together. We also figured out you could get multiple items from one photograph by color correcting one color to look like others, of course that only works if the fabric pattern matched.</p>
         <p>These advances helped but we were still going to need weeks of photography and photo editing. We would also be locked into a specific layout. If we ever wanted to change it, we'd be stuck re-shooting everything. This was a small in-house team with other responsibilities, that wasn't going to work.</p>
-      </div>
+      </ContentCard>
 
-      <div className="bg-white rounded-xl sm:rounded-3xl shadow-2xl p-32 sm:px-64 sm:pt-64 sm:pb-72 space-y-32 w-full max-w-3xl mx-auto">
+      <ContentCard>
         <h3>How do other companies do this?</h3>
         <p>We started digging and came across the answer pretty quickly: 3D renderings. A great example is Nike's shoe builder. I had some experience with 3D but nothing that I thought would pass as photo realistic for a clothing company. I also still had the issue of creating hundreds of individual assets.</p>
         <p>There were some benefits to 3D rendering though. We could change camera angles and lighting, we could improving the quality over time, and base 3d models could be reused for different material types. We decided to give it a shot.</p>
-      </div>
+      </ContentCard>
 
       <div className="case-study-grid-images w-full max-w-[1182px] mx-auto">
         <div style={{ gridRow: "1/3" }}>
@@ -139,7 +140,7 @@ const LookBuilder = () => (
         </div>
       </div>
 
-      <div className="bg-white rounded-xl sm:rounded-3xl shadow-2xl p-32 sm:px-64 sm:pt-64 sm:pb-72 space-y-32 w-full max-w-3xl mx-auto">
+      <ContentCard>
         <h3>3D for the Win!</h3>
         <p>I made some exciting discoveries.</p>
         <ol>
@@ -149,7 +150,7 @@ const LookBuilder = () => (
         <p>
           My 3D application of choice is <a href="https://www.blender.org/">Blender</a>. Through this exploration process, I learned about some of its powerful automation capabilities. We could use Python and the command line to render assets. And because Blender is open source, I could render on any computer or multiple computers without having to worry about licenses. That meant we could easily scale up the pipeline if we needed to re-render a lot of items.
         </p>
-      </div>
+      </ContentCard>
 
       <div className="case-study-grid-images w-full max-w-[1182px] mx-auto">
         <div style={{ gridRow: "1/3", gridColumn: "1/3" }}>
@@ -160,12 +161,12 @@ const LookBuilder = () => (
         </div>
       </div>
 
-      <div className="bg-white rounded-xl sm:rounded-3xl shadow-2xl p-32 sm:px-64 sm:pt-64 sm:pb-72 space-y-32 w-full max-w-3xl mx-auto">
+      <ContentCard>
         <h4> The Rendering Pipeline</h4>
         <p>I ended up creating an entire 3D rendering tool set. We had a master file for all the 3D fabric materials, files for each of the 3D models, and a master scene file that contained the studio setup. We got to the point where we could open terminal, type a line of code with the object name (e.g. bow tie) and material (e.g. pin dot fabric) and the computer would start spitting out images in the background!</p>
-      </div>
+      </ContentCard>
 
-      <div className="bg-white rounded-xl sm:rounded-3xl shadow-2xl p-32 sm:px-64 sm:pt-64 sm:pb-72 space-y-32 w-full max-w-3xl mx-auto">
+      <ContentCard>
         <h2 id="process">UI Design Process</h2>
         <hr className="border-gray-300" />
         <p>The design process for this project was a ton of fun. We experimented a lot, shot for the moon, then backed off a little for something a little more reserved but functionally solid and user friendly.</p>
@@ -176,7 +177,7 @@ const LookBuilder = () => (
           <li>Static Prototyping (non-code based prototypes aimed at working out high level user flow or micro interactions) </li>
           <li>Mini user testing with friends, co-workers, family members</li>
         </ul>
-      </div>
+      </ContentCard>
 
       <div>
         <div className="w-full max-w-[1182px] mx-auto">
@@ -252,13 +253,13 @@ const LookBuilder = () => (
         <video className="lazy-load rounded-xl sm:rounded-3xl mx-auto" loop muted controls preload="metadata" data-src="/assets/images/lookbuilder-hero.mp4#t=0.1" alt={{ title }}></video>
       </div>
 
-      <div className="bg-white rounded-xl sm:rounded-3xl shadow-2xl p-32 sm:px-64 sm:pt-64 sm:pb-72 space-y-32 w-full max-w-3xl mx-auto">
+      <ContentCard>
         <h2 id="prototyping">Prototyping</h2>
         <hr className="border-gray-300" />
 
         <h3>Pedal to the Metal!</h3>
         <p>Once we felt like things were starting to mesh and we had stakeholder sign off on the feature set, we began building a fully functional, data backed prototype with React. At this point, there's still a ton of design refinement happening. Many details of a design don't really make sense until you get them working in code.</p>
-      </div>
+      </ContentCard>
 
       <div className="video-callout w-full max-w-[400px] mx-auto">
         <video className="lazy-load rounded-xl sm:rounded-3xl mx-auto " data-src="/assets/images/200402-194656.mp4#t=0.1" muted controls loop playsInline preload="metadata"></video>
@@ -274,7 +275,7 @@ const LookBuilder = () => (
         </Button>
       </div>
 
-      <div className="bg-white rounded-xl sm:rounded-3xl shadow-2xl p-32 sm:px-64 sm:pt-64 sm:pb-72 space-y-32 w-full max-w-3xl mx-auto">
+      <ContentCard>
         <h3>User Testing</h3>
         <p>For the final stage, we conducted formal user testing with an online service that let us to supply a link to the prototype, specify demographics and devices, then initialize a test. No matter how hard you try to think from the user's perspective, you always get things wrong. It continues to amaze me how much you learn from watching people use an app or website.</p>
         <h4> Here were a few key findings from the tests:</h4>
@@ -284,13 +285,13 @@ const LookBuilder = () => (
           <li>Some users accidentally canceled their changes by hitting the x button. </li>
           <li>Editing looks was a little tough for a few users.</li>
         </ol>
-      </div>
+      </ContentCard>
 
       <div className="video-callout w-full max-w-[1182px] mx-auto">
         <img className="lazy-load rounded-xl sm:rounded-3xl mx-auto" src="/assets/images/loading.png" data-src="/assets/images/200330-172519.jpg" />
       </div>
 
-      <div className="bg-white rounded-xl sm:rounded-3xl shadow-2xl p-32 sm:px-64 sm:pt-64 sm:pb-72 space-y-32 w-full max-w-3xl mx-auto">
+      <ContentCard>
         <h4> We addressed the issues with the following changes:</h4>
         <ol>
           <li>Added back button </li>
@@ -298,9 +299,9 @@ const LookBuilder = () => (
           <li>Reduced the x button size and made it a back button. We also change the behavior to not cancel changes. </li>
           <li>Added an edit icon in place of the price. This also aligned new and edit function buttons.</li>
         </ol>
-      </div>
+      </ContentCard>
 
-      <div className="bg-white rounded-xl sm:rounded-3xl shadow-2xl p-32 sm:px-64 sm:pt-64 sm:pb-72 space-y-32 w-full max-w-3xl mx-auto">
+      <ContentCard>
         <h2 id="conclusion">Conclusion</h2>
         <hr className="border-gray-300" />
         <p>The final code base was handed off to the engineering team to integrate into the Gentux website. In the end, the UI team accomplished a staggering amount of great work over a relatively small amount of time.</p>
@@ -311,7 +312,7 @@ const LookBuilder = () => (
           <li>3D rendering pipeline that can automate the creation of thousands of assets. </li>
           <li>Modular NPM package that can be easily imported to multiple projects.</li>
         </ul>
-      </div>
+      </ContentCard>
 
       <div className="w-full max-w-3xl mx-auto">
         <Button target="_blank" href="https://www.generationtux.com/app/customize?bundle_ids=112&sidecar=list&product_skus=330914WHT%2C911246PLM%2C430464BLW%2C610158WHT%2C030088SLV%2CB10903BWN%2CA10186BRA%2C881007BLB">
