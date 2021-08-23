b421572d654aa9167a7b8132c607c56ab2fe98bc
Patrick Hill <patrickrhill@gmail.com>
2021-08-23 08:28:24 -0400

    tighter spacing on mobile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => (
           </div>
           <div>
             <div className="max-w-2xl rounded-[1.1rem] sm:rounded-[1.9rem] bg-gradient-brand p-2">
-              <a href="/casestudies/lookbuilder" className={`p-24 rounded-[1.05rem] sm:rounded-[1.85rem] bg-gray-800 !overflow-visible block hover-3d   hover:bg-gray-700 transition duration-300`}>
+              <a href="/casestudies/lookbuilder" className={`p-16 sm:p-24 rounded-[1.05rem] sm:rounded-[1.85rem] bg-gray-800 !overflow-visible block hover-3d   hover:bg-gray-700 transition duration-300`}>
                 <div style={{ perspective: 800 }}>
                   <div className="hover-3d-target transition duration-[300ms] ease-out will-change">
                     <video className={`${roundedClassNames} w-full`} loop autoPlay playsInline muted src="/assets/images/200504-093605.mp4" alt="Look Builder UI" />
