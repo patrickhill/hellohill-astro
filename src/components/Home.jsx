f9b9e3ef8f3c479d85295854d93f74da224f04e8
Patrick Hill <patrickrhill@gmail.com>
2021-11-09 13:08:31 -0500

    swaps case study order

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -27,6 +27,24 @@ const Home = () => (
             <LineVertical className="flex-grow" />
           </div>
           <div className="space-y-64">
+  
+           <div className="max-w-2xl rounded-[1.1rem] sm:rounded-[1.9rem] bg-gradient-brand p-2">
+              <a href="/casestudies/lookbuilder" className={`p-16 sm:p-24 rounded-[1.05rem] sm:rounded-[1.85rem] bg-gray-800 !overflow-visible block hover-3d   hover:bg-gray-700 transition duration-300`}>
+                <div style={{ perspective: 800 }}>
+                  <div className="hover-3d-target transition duration-[300ms] ease-out will-change">
+                    <video className={`${roundedClassNames} w-full`} loop autoPlay playsInline muted src="/assets/images/200504-093605.mp4" alt="Look Builder UI" />
+                  </div>
+                </div>
+                <div className="flex justify-between mt-32 space-x-8">
+                  <div>
+                    <SubHeading as="h2">The Look Builder</SubHeading>
+                    <p className="text-white text-base font-light">Visualizing custom suit combinations</p>
+                  </div>
+                  <Button>Read</Button>
+                </div>
+              </a>
+            </div>
+            
             <div className="max-w-2xl rounded-[1.1rem] sm:rounded-[1.9rem] bg-gray-700 p-2">
               <div className={`p-16 sm:p-24 rounded-[1.05rem] sm:rounded-[1.85rem] bg-gray-800 !overflow-visible block hover:cursor-auto`}>
                 <div style={{ perspective: 800 }}>
@@ -44,22 +62,7 @@ const Home = () => (
               </div>
             </div>
 
-            <div className="max-w-2xl rounded-[1.1rem] sm:rounded-[1.9rem] bg-gradient-brand p-2">
-              <a href="/casestudies/lookbuilder" className={`p-16 sm:p-24 rounded-[1.05rem] sm:rounded-[1.85rem] bg-gray-800 !overflow-visible block hover-3d   hover:bg-gray-700 transition duration-300`}>
-                <div style={{ perspective: 800 }}>
-                  <div className="hover-3d-target transition duration-[300ms] ease-out will-change">
-                    <video className={`${roundedClassNames} w-full`} loop autoPlay playsInline muted src="/assets/images/200504-093605.mp4" alt="Look Builder UI" />
-                  </div>
-                </div>
-                <div className="flex justify-between mt-32 space-x-8">
-                  <div>
-                    <SubHeading as="h2">The Look Builder</SubHeading>
-                    <p className="text-white text-base font-light">Visualizing custom suit combinations</p>
-                  </div>
-                  <Button>Read</Button>
-                </div>
-              </a>
-            </div>
+            
           </div>
         </div>
       </div>
