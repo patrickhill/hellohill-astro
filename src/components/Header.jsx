44e52c16eb9a7b8b17f15e1f8c5bb3d45ea4dba4
Patrick Hill <patrickrhill@gmail.com>
2021-11-24 08:20:41 -0500

    adds hove ranimation to logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import Button from "../components/Button";
 
 const Header = () => (
   <>
-    <div className="container flex justify-between items-center p-32 pb-80 md:pb-128">
-      <a href="/">
+    <div className="container flex items-center justify-between p-32 pb-80 md:pb-128">
+      <a href="/" className="p-8 -ml-8 transition duration-500 ease-in-out rounded-full hover:bg-gray-600 hover:rotate-180">
         <img className="inline-block" src="/assets/images/logo.svg" alt="Hello Hill Logo" />
       </a>
 
