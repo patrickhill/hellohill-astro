6cc096036813872e6c0b72272302004cb61a078c
Patrick Hill <patrickrhill@gmail.com>
2022-02-12 17:36:13 -0500

    converts logo to svg jsx component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Button from "../components/Button";
+import Logo from "../components/Logo";
 
 const Header = () => (
   <>
     <div className="container flex items-center justify-between p-32 pb-80 md:pb-128">
-      <a href="/" className="p-8 -ml-8 transition duration-500 ease-in-out rounded-full hover:bg-gray-600 hover:rotate-180">
-        <img className="inline-block" src="/assets/images/logo.svg" alt="Hello Hill Logo" />
+      <a href="/" className="-ml-8 rounded-full p-8 transition duration-500 ease-in-out hover:rotate-180 hover:bg-gray-600">
+        <Logo />
       </a>
 
       <Button href="#footer">Say Hello</Button>
