821af4d0fae4692ff9e4b5259911d9efd069e73d
Patrick Hill <patrickrhill@gmail.com>
2022-02-14 11:00:01 -0500

    adjusts scroll activation point

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
   if (intersectionObserverSupported) {
     let options = {
       root: null,
-      rootMargin: "0px 0px 10%",
+      rootMargin: "0px 0px 50%",
       threshold: 0,
     };
     let isIntersecting = (entries, observer) => {
